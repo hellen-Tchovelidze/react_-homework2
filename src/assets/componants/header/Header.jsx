428b30d14a82723eb8e511193eaa6dcb17a9951c
hellen-Tchovelidze <elenechovelidze@gmail.com>
2025-01-14 21:28:53 +0400

    fix(header css) fixed hover

--- src/assets/componants/header/Header.jsx
+++ src/assets/componants/header/Header.jsx
@@ -1,11 +1,15 @@
 import "./Header.css"
 import photo from "../images/Rectangle Copy (1).png"
-
+import icone from "../images/Eye.png"
 
 function Header() {
     return(
         <>
-<img src={photo} alt="Sample"  className="photo"/>
+        
+        <div style={{ position: "relative", display: "inline-block" }}>
+                <img src={photo} alt="Sample" className="photo" />
+               <img src={icone} alt="Eye Icon" className="icone" />
+           </div>
 
 <h1 className="heading">Equilibrium #3429</h1>
         </>
